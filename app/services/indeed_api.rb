8f2d1258a4ce9d4073b0e06c96a0e6fa9d2f49b7
Dimitri Bosch <dimitribosch@hotmail.com>
2016-11-23 15:02:45 +0900

    making progress inside the indeed service. but still need some work

--- app/services/indeed_api.rb
+++ app/services/indeed_api.rb
@@ -6,36 +6,57 @@ class IndeedApi
   def initialize(attributes = {})
     @publisher_key = attributes[:publisher_key]
     @categories = attributes[:categories]
-    @locations = attributes[:locations] || [["paris", "fr"]]
+    @locations = attributes[:locations] || ["paris", "fr"]
   end
 
   def get_jobs
-    # iterate on @categories & locations
     # call the api with parameter, for each category
+    results = []
     @categories.each do |category|
-      @locations.each do |location|
-        call_api(category.name, location)
-      end
+      p category
+      results << call_api(category.name)
     end
     # create a job_offer instace for each result
+    results.each do |result|
+      # p result['query']
+      p result.class
+      p result.length
+    end
+
     # return something (true?)
     # handle error with raise and begin
   end
 
   private
 
-  def call_api(category, location)
-    params = {
-      category: category,
-      city: location[0],
-      country: location[1],
-      limit: 9,
-    }
+  def call_api(category)
+    begin
+      params = {
+        category: category,
+        city: @locations[0],
+        country: @locations[1],
+        limit: 2,
+      }
 
-    url = "http://api.indeed.com/ads/apisearch?publisher=#{@publisher_key}&q=#{params[:category]}&l=#{params[:city]}&sort=&radius=&st=&jt=&start=&limit=#{params[:limit]}&fromage=&filter=&latlong=1&co=#{params[:country]}&format=json&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2"
-    offers_serialized = open(url).read
-    offers = JSON.parse(offers_serialized)
-    p offers
+      url = "http://api.indeed.com/ads/apisearch?publisher=#{@publisher_key}&q=#{params[:category]}&l=#{params[:city]}&sort=&radius=&st=&jt=&start=&limit=#{params[:limit]}&fromage=&filter=&latlong=1&co=#{params[:country]}&format=json&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2"
+      offers_serialized = open(url).read
+      offers = JSON.parse(offers_serialized)
+      fail unless offers['results']
+    rescue => e
+      puts "***** An error occurred call_api with message #{e.message}: retrying in 5 seconds - category: #{params[:category]}*****"
+      sleep 2
+      retry
+    end
+    return offers['results']
+  end
+
+  def job_instances(offer)
+    # p offer.class
+    # offer.each do { |key, _value| p key }
+  end
+
+  def method_name
+
   end
 
 end
