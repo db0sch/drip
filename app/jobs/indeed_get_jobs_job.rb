3b9ed70a8dc49d1bd63fdcda15d8029d1fc02ffe
Dimitri Bosch <dimitribosch@hotmail.com>
2016-11-24 18:23:15 +0900

    the job is written. even though a new bug has been found in the indeed service. fix #8

--- app/jobs/indeed_get_jobs_job.rb
+++ app/jobs/indeed_get_jobs_job.rb
@@ -1,7 +1,11 @@
 class IndeedGetJobsJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform
+    indeed = IndeedApi.new(
+      publisher_key: ENV['indeed_publisher_key'],
+      categories: Category.to_array,
+    )
+    indeed.get_jobs
   end
 end
