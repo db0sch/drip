e78e4fbcd6db627b0b2afc47d972b0d8f178ed93
Dimitri Bosch <dimitribosch@hotmail.com>
2016-11-25 17:56:54 +0900

    refacto of the api call. simpler now. Still need some work

--- app/services/indeed_api.rb
+++ app/services/indeed_api.rb
@@ -11,21 +11,27 @@ class IndeedApi
 
   def get_jobs
     # call the api with parameter, for each category
-    results_by_category = {}
+    job_offers_attributes = []
     @categories.each do |category|
-      results_by_category[category] = call_api_search(category.name)
+      job_offers_attributes << call_api_search(category)
     end
-    # create a job_offer instance for each result
-    job_offers = []
-    results_by_category.each do |category, results|
-      results.each do |result|
-        unless JobOffer.find_by_jobkey(result["jobkey"])
-          job_offers << create_job_offers(result, category)
-        end
-      end
+
+    job_offers_attributes.flatten.map! do |result|
+      call_api_getjob(result)
     end
-    # update joboffer with a new call to the API (Get_Job Indeed API)
-    job_offers.compact.each { |job_offer| update_infos(job_offer) if job_offer.url_source_original } if job_offers.any?
+
+    # p job_offers_attributes
+    # # create a job_offer instance for each result
+    # job_offers = []
+    # results_by_category.each do |category, results|
+    #   results.each do |result|
+    #     unless JobOffer.find_by_jobkey(result["jobkey"])
+    #       job_offers << create_job_offers(result, category)
+    #     end
+    #   end
+    # end
+    # # update joboffer with a new call to the API (Get_Job Indeed API)
+    # job_offers.compact.each { |job_offer| update_infos(job_offer) if job_offer.url_source_original } if job_offers.any?
   end
 
   private
@@ -33,7 +39,7 @@ class IndeedApi
   def call_api_search(category)
     begin
       params = {
-        category: category,
+        category: category.name,
         city: @locations[0],
         country: @locations[1],
         limit: 30,
@@ -41,27 +47,53 @@ class IndeedApi
       url = "http://api.indeed.com/ads/apisearch?publisher=#{@publisher_key}&q=#{params[:category]}&l=#{params[:city]}&sort=&radius=&st=&jt=&start=&limit=#{params[:limit]}&fromage=&filter=&latlong=1&co=#{params[:country]}&format=json&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2"
       offers_serialized = open(url).read
       offers = JSON.parse(offers_serialized)
+
       fail unless offers['results']
+
+      offers_attributes = offers['results'].map do |offer|
+        attributes = {
+          title: offer["jobtitle"],
+          company: find_or_create_company(offer["company"]),
+          description: offer["snippet"],
+          formatted_location: offer["formattedLocation"],
+          city: offer["city"],
+          country: offer["country"],
+          date: DateTime.parse(offer["date"]),
+          source_primary: "indeed",
+          source_original: offer["source"],
+          url_source_primary: offer["url"],
+          jobkey: offer["jobkey"],
+          category: category
+        }
+      end
     rescue => e
       puts "***** An error occurred call_api with message #{e.message}: retrying in 5 seconds - category: #{params[:category]}*****"
       sleep 2
       retry
     end
-    return offers['results']
+    offers_attributes
   end
 
-  def call_api_getjob(jobkey)
+  def call_api_getjob(job_offer_attributes)
+    p job_offer_attributes.class
     begin
-      url = "http://api.indeed.com/ads/apigetjobs?publisher=#{@publisher_key}&jobkeys=#{jobkey}&v=2&format=json"
-      offers_serialized = open(url).read
-      offers = JSON.parse(offers_serialized)
-      fail unless offers['results']
+      url = "http://api.indeed.com/ads/apigetjobs?publisher=#{@publisher_key}&jobkeys=#{job_offer_attributes[:jobkey]}&v=2&format=json"
+      offer_serialized = open(url).read
+      offer = JSON.parse(offer_serialized)
+      fail unless offer['results']
+      p offer
+      p offer['results']
+      p offer['results'].first['url']
+      job_offer_attributes[:description_additional] = offer['results'].first['snippet']
+      job_offer_attributes[:url_source_original] = offer['results'].first['url']
+      job_offer_attributes[:expired] = offer['results'].first['expired']
+
     rescue => e
-      puts "***** An error occurred call_api with message #{e.message}: retrying in 5 seconds - jobkey: #{jobkey}*****"
+      puts "***** An error occurred call_api with message #{e.message}: retrying in 5 seconds - jobkey: #{job_offer_attributes[:jobkey]}*****"
       sleep 2
       retry
     end
-    return offers['results']
+    job_offer_attributes
   end
 
   def update_infos(job_offer)
