0eb7c01b22e4a2e156cfd8743aabf8d31e0d5060
Dimitri Bosch <dimitribosch@hotmail.com>
2016-11-25 16:59:42 +0900

    some refacto and bug correction

--- app/services/indeed_api.rb
+++ app/services/indeed_api.rb
@@ -15,15 +15,17 @@ class IndeedApi
     @categories.each do |category|
       results_by_category[category] = call_api_search(category.name)
     end
-    # create a job_offer instace for each result
+    # create a job_offer instance for each result
     job_offers = []
     results_by_category.each do |category, results|
       results.each do |result|
-        job_offers << create_job_offers(result, category)
+        unless JobOffer.find_by_jobkey(result["jobkey"])
+          job_offers << create_job_offers(result, category)
+        end
       end
     end
-    p job_offers
-    job_offers.compact.each { |job_offer| update_infos(job_offer) } if job_offers.any?
+    # update joboffer with a new call to the API (Get_Job Indeed API)
+    job_offers.compact.each { |job_offer| update_infos(job_offer) if job_offer.url_source_original } if job_offers.any?
   end
 
   private
@@ -34,7 +36,7 @@ class IndeedApi
         category: category,
         city: @locations[0],
         country: @locations[1],
-        limit: 20,
+        limit: 30,
       }
       url = "http://api.indeed.com/ads/apisearch?publisher=#{@publisher_key}&q=#{params[:category]}&l=#{params[:city]}&sort=&radius=&st=&jt=&start=&limit=#{params[:limit]}&fromage=&filter=&latlong=1&co=#{params[:country]}&format=json&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2"
       offers_serialized = open(url).read
@@ -64,34 +66,33 @@ class IndeedApi
 
   def update_infos(job_offer)
     result = call_api_getjob(job_offer.jobkey).first
-    args = {
-      description_additional: result['snippet'],
-      url_source_original: result['url'],
-      expired: result['expired']
-    }
-    job_offer.update(args) if result
+    if result
+      args = {
+        description_additional: result['snippet'],
+        url_source_original: result['url'],
+        expired: result['expired']
+      }
+      job_offer.update(args)
+    end
   end
 
   def create_job_offers(offer, category)
-
-    unless JobOffer.find_by_jobkey(offer["jobkey"])
-      puts "creating job: #{offer["jobtitle"]} for company: #{offer["company"]}"
-      args = {
-        title: offer["jobtitle"],
-        company: find_or_create_company(offer["company"]),
-        category: category,
-        description: offer["snippet"],
-        formatted_location: offer["formattedLocation"],
-        city: offer["city"],
-        country: offer["country"],
-        date: DateTime.parse(offer["date"]),
-        source_primary: "indeed",
-        source_original: offer["source"],
-        url_source_primary: offer["url"],
-        jobkey: offer["jobkey"]
-      }
-      job = JobOffer.create(args)
-    end
+    puts "creating job: #{offer["jobtitle"]} for company: #{offer["company"]}"
+    args = {
+      title: offer["jobtitle"],
+      company: find_or_create_company(offer["company"]),
+      category: category,
+      description: offer["snippet"],
+      formatted_location: offer["formattedLocation"],
+      city: offer["city"],
+      country: offer["country"],
+      date: DateTime.parse(offer["date"]),
+      source_primary: "indeed",
+      source_original: offer["source"],
+      url_source_primary: offer["url"],
+      jobkey: offer["jobkey"]
+    }
+    job = JobOffer.create(args)
   end
 
   def find_or_create_company(name)
