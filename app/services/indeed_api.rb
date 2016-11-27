4a7b72867c89bceaf03960205b650214e999843c
Dimitri Bosch <dimitribosch@hotmail.com>
2016-11-27 19:24:42 +0900

    refacto of the indeedapi service code. clearer. Less db call except for companies

--- app/services/indeed_api.rb
+++ app/services/indeed_api.rb
@@ -7,35 +7,29 @@ class IndeedApi
     @publisher_key = attributes[:publisher_key]
     @categories = attributes[:categories]
     @locations = attributes[:locations] || ["paris", "fr"]
+    @limit = attributes[:limit]
   end
 
   def get_jobs
     # call the api with parameter, for each category
-    job_offers_attributes = []
-    @categories.each do |category|
-      job_offers_attributes << call_api_search(category)
+    array_of_attributes = @categories.map do |category|
+      call_api_search(category)
     end
+
+    # reject all job_offers which are already in the database (comparing jobkeys)
+    jobkeys = get_all_jobkeys
+    array_of_attributes.flatten!.reject! do |attributes|
+      jobkeys.include? attributes[:jobkey]
+    end
+
     # call the get job api, and update the attributes hashes
-    job_offers_attributes.flatten.map! do |result|
+    array_of_attributes.map! do |result|
       call_api_getjob(result)
     end
 
-    # Once we've got the hashes completed.
-    # We can create JobOffers simply.
-
-
-    # p job_offers_attributes
-    # # create a job_offer instance for each result
-    # job_offers = []
-    # results_by_category.each do |category, results|
-    #   results.each do |result|
-    #     unless JobOffer.find_by_jobkey(result["jobkey"])
-    #       job_offers << create_job_offers(result, category)
-    #     end
-    #   end
-    # end
-    # # update joboffer with a new call to the API (Get_Job Indeed API)
-    # job_offers.compact.each { |job_offer| update_infos(job_offer) if job_offer.url_source_original } if job_offers.any?
+    # create JobOffer instances from these attributes.
+    create_job_offers_instances_with array_of_attributes
+    # the method will return JobOffer instances.
   end
 
   private
@@ -46,9 +40,8 @@ class IndeedApi
         category: category.name,
         city: @locations[0],
         country: @locations[1],
-        limit: 30,
       }
-      url = "http://api.indeed.com/ads/apisearch?publisher=#{@publisher_key}&q=#{params[:category]}&l=#{params[:city]}&sort=&radius=&st=&jt=&start=&limit=#{params[:limit]}&fromage=&filter=&latlong=1&co=#{params[:country]}&format=json&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2"
+      url = "http://api.indeed.com/ads/apisearch?publisher=#{@publisher_key}&q=#{params[:category]}&l=#{params[:city]}&sort=&radius=&st=&jt=&start=&limit=#{@limit}&fromage=&filter=&latlong=1&co=#{params[:country]}&format=json&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2"
       offers_serialized = open(url).read
       offers = JSON.parse(offers_serialized)
 
@@ -79,12 +72,12 @@ class IndeedApi
   end
 
   def call_api_getjob(job_offer_attributes)
-    p job_offer_attributes.class
     begin
       url = "http://api.indeed.com/ads/apigetjobs?publisher=#{@publisher_key}&jobkeys=#{job_offer_attributes[:jobkey]}&v=2&format=json"
       offer_serialized = open(url).read
       offer = JSON.parse(offer_serialized)
       fail unless offer['results']
+      p "updating job_offer #{job_offer_attributes[:jobkey]}"
       job_offer_attributes[:description_additional] = offer['results'].first['snippet']
       job_offer_attributes[:url_source_original] = offer['results'].first['url']
       job_offer_attributes[:expired] = offer['results'].first['expired']
@@ -94,37 +87,6 @@ class IndeedApi
       retry
     end
     job_offer_attributes
-  end
-
-  def update_infos(job_offer)
-    result = call_api_getjob(job_offer.jobkey).first
-    if result
-      args = {
-        description_additional: result['snippet'],
-        url_source_original: result['url'],
-        expired: result['expired']
-      }
-      job_offer.update(args)
-    end
-  end
-
-  def create_job_offers(offer, category)
-    puts "creating job: #{offer["jobtitle"]} for company: #{offer["company"]}"
-    args = {
-      title: offer["jobtitle"],
-      company: find_or_create_company(offer["company"]),
-      category: category,
-      description: offer["snippet"],
-      formatted_location: offer["formattedLocation"],
-      city: offer["city"],
-      country: offer["country"],
-      date: DateTime.parse(offer["date"]),
-      source_primary: "indeed",
-      source_original: offer["source"],
-      url_source_primary: offer["url"],
-      jobkey: offer["jobkey"]
-    }
-    job = JobOffer.create(args)
   end
 
   def find_or_create_company(name)
@@ -137,4 +99,21 @@ class IndeedApi
     end
   end
 
+  def get_all_jobkeys
+    JobOffer.pluck(:jobkey)
+  end
+
+  def create_job_offers_instances_with(bunch_of_attributes)
+    instances = []
+    bunch_of_attributes.each do |attr|
+      joboffer = JobOffer.new(attr)
+      if joboffer.save
+        instances << joboffer
+        puts "Job_offer #{attr[:jobkey]} has been created"
+      else
+        puts "Job_offer #{attr[:jobkey]} has not been created because #{joboffer.errors.messages}"
+      end
+    end
+    instances
+  end
 end
